46dc6c965acafd810112c117eac83a207c6f61de
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-02-22 23:47:44 -0300

    Troca de valores

---
+++ Entrada e saída de dados/Troca_de_valores.py
@@ -0,0 +1,5 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+a, b = b, a
+print(f"Após a troca, o primeiro número é: {a}")
+print(f"Após a troca, o segundo número é: {b}")
