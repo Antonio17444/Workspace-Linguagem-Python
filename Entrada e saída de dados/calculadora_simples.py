fcaaa98396639180788e8f3a7f7e120af554d79b
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-02-23 19:12:43 -0300

    Calculadora Simples

---
+++ Entrada e saída de dados/calculadora_simples.py
@@ -0,0 +1,19 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operador = input("Digite o operador (+, -, *, /): ")
+
+if operador == '+':
+    resultado = num1 + num2
+elif operador == '-':
+    resultado = num1 - num2
+elif operador == '*':
+    resultado = num1 * num2
+elif operador == '/':
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Erro: divisão por zero!"
+else:
+    resultado = "Operador inválido!"
+
+print(f"Resultado: {resultado}")
